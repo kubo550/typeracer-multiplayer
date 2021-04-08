9ada42b8cc78947b66151789c6d0df340d549a5b
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-04-08 22:18:47 +0200

    Create Pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,18 @@
+const About = () => {
+  return (
+    <div>
+      <div>
+        Icons made by
+        <a href='' title='Adib Sulthon'>
+          Adib Sulthon
+        </a>
+        from
+        <a href='https://www.flaticon.com/' title='Flaticon'>
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,82 @@
+import {
+  Button,
+  Container,
+  FormGroup,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import honda from "../assets/honda-car.png";
+
+// Todo Kurwa może by uzyc react-hook-form
+
+const Home = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  const history = useHistory();
+
+  const handleJoinRoom = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+
+    if (!name || !room) {
+      return;
+    }
+
+    history.push(`/race?name=${name}&room=${room}`);
+  };
+
+  return (
+    <Container maxWidth='xs'>
+      <Paper elevation={10} style={{ padding: "20px" }}>
+        <Typography variant='h5' align='center'>
+          Type Racer Clone
+        </Typography>
+
+        <FormGroup>
+          <TextField
+            label='Name'
+            fullWidth
+            value={name}
+            onChange={e => setName(e.target.value)}
+            // error={true}
+            // helperText={"kurwa ale błąd"}
+          />
+
+          <TextField
+            label='Room'
+            fullWidth
+            value={room}
+            onChange={e => setRoom(e.target.value)}
+          />
+
+          <br />
+          <Button variant='contained' color='primary' onClick={handleJoinRoom}>
+            Join Race
+          </Button>
+
+          <Typography variant='h6' align='center'>
+            OR
+          </Typography>
+
+          <Link to='race' style={{ textDecoration: "none" }}>
+            <Button variant='contained' color='secondary' fullWidth>
+              Play Solo
+            </Button>
+          </Link>
+        </FormGroup>
+      </Paper>
+      <img
+        src={honda}
+        alt='Honda car'
+        style={{ width: "145%", marginLeft: "-20%" }}
+      />
+    </Container>
+  );
+};
+
+export default Home;
