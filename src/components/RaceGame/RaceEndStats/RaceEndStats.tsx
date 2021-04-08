7adc0b1cf56519dc040f8147c9805ebef20df949
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-04-09 00:05:40 +0200

    Create Stats Layout

--- src/components/RaceGame/RaceEndStats/RaceEndStats.tsx
+++ src/components/RaceGame/RaceEndStats/RaceEndStats.tsx
@@ -1,30 +1,69 @@
-import { Button, Grid, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import { FC } from "react";
 import { SongApiObject } from "../../../../../routes/texts";
 import { Bordered, COLORS } from "../../../layout";
+import { displayTime } from "../RaceGame.helper";
 
-interface RaceEndStatsProps {
+import * as S from "./RaceEndStats.style";
+
+import keyboard from "../../../assets/keyboard.png";
+import check from "../../../assets/check.png";
+import timer from "../../../assets/timer.png";
+
+interface RaceStatProps {
   newGame: () => void;
   wps: number;
   time: number | null;
   details: SongApiObject;
 }
 
-const RaceEndStats: FC<RaceEndStatsProps> = ({
-  newGame,
-  wps,
-  time,
-  details,
-}) => {
+const RaceEndStats: FC<RaceStatProps> = ({ newGame, wps, time, details }) => {
   return (
     <Bordered color={COLORS.orange}>
-      Speed: {wps}
-      <br />
-      Time: {time}
-      <br />
-      Autor: {details.author}
-      <br />
-      {details.title}
+      <S.Header>
+        <Typography variant='body1'>
+          You just typed a quote from the song:
+        </Typography>
+      </S.Header>
+
+      <S.Wrapper>
+        <S.ImgContainer>
+          <img src={details.image} alt={details.title} />
+        </S.ImgContainer>
+
+        <S.Details>
+          <S.Author>
+            <Typography variant='h5'>
+              <a href={details.link}> {details.title} </a>
+            </Typography>
+            <Typography variant='h6'>By {details.author} </Typography>
+          </S.Author>
+
+          <S.StatsTable>
+            <div>
+              <div>
+                <img src={keyboard} alt='Keyboard icon' /> Speed:
+              </div>
+              <div> {wps.toFixed(2)} </div>
+            </div>
+
+            <div>
+              <div>
+                <img src={timer} alt='Timer icon' />
+                Time:
+              </div>
+              <div> {displayTime(time)} </div>
+            </div>
+
+            <div>
+              <div>
+                <img src={check} alt='Accuracy icon' /> Accuracy:
+              </div>
+              <div> 97.43 % </div>
+            </div>
+          </S.StatsTable>
+        </S.Details>
+      </S.Wrapper>
     </Bordered>
   );
 };
